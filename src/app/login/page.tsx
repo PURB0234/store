37d5800257292c8e729c42916a6edd99fc41a2f1
unknown <purbojatmiko261@gmail.com>
2025-07-20 17:33:32 +0700

    oi

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -32,7 +32,7 @@ export default function Login() {
       {err && <p className="text-red-500">{err}</p>}
       <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} className="border p-2" required/>
       <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} className="border p-2" required/>
-      <button type="submit" className="bg-blue-600 text-white p-2 rounded">Login</button>
+      <button type="submit" className="bg-blue-600 text-white p-2 rounded">Memek sindi enak</button>
       <Link className="text-black" href="/register">Register</Link>
     </form>
   );
